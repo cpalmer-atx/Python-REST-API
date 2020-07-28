f648cf677a1cf47e12fd35c7ac27fa02ca71646a
Chad Palmer <chad.n.palmer@gmail.com>
2020-07-28 16:23:58 -0500

    cleaned up boilerplate for Python REST API using Flask

--- app.py
+++ app.py
@@ -1,53 +1,72 @@
-# https://flask-restful.readthedocs.io/en/latest/api.html
-
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 
 app = Flask(__name__)
-app.secret_key = 'chad'         # obviously this isn't how we'd call our key...    
+app.secret_key = 'chad'                 # obviously this isn't how we'd call our key...
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)  # /auth
 
 items = []
 
-# Api works with resources, and every resource is a class.
-# In this example, Student inherits from Resource and then we
-# redefine the built-in get() function.
-
 class Item(Resource):
-    def get(self, name):
-        
-        # for item in items:
-        #     if item['name'] == name:
-        #         return item
 
-        # this lambda function replaces the above for loop.
-        # filter returns a filter object, so we call list() and
-        # pass filter for desirable output.  Since there should only
-        # be one item in the query, we wrap list in next() to only
-        # return first item that matches query. Next() can break our
-        # code if the list is empty, so None is also used as a default value.
-        item = next(iter(filter(lambda x: x['name'] == name, items)), None)
+    # Tool to parse all requests
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="Item price cannot be left blank."
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
 
         # If item not in database:
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        if next(iter(filter(lambda x: x['name'] == name, items)), None) is not None:
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
 
+        data = Item.parser.parse_args()     # calling parser tool defined at begining of Item class
+
         # Get JSON payload from request
-        data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+
+        # Function overwrites existing items list excluding item to delete
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': '{} deleted'.format(name)}
+
+    def put(self, name):
+
+        data = Item.parser.parse_args()     # calling parser tool defined at begining of Item class
+        item = next(filter(lambda x: x['name'] == name, items), None)
+
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+            
 class ItemList(Resource):
     def get(self):
         return {'items': items}
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
